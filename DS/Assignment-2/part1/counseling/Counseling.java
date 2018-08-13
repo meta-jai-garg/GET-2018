f683da18ee258da4581fb9549962e0860b017e2a
meta-jai-garg <41181707+meta-jai-garg@users.noreply.github.com>
2018-08-13 10:47:47 +0530

    Update Counseling.java

--- DS/Assignment-2/part1/counseling/Counseling.java
+++ DS/Assignment-2/part1/counseling/Counseling.java
@@ -34,25 +34,20 @@ public class Counseling {
 			}
 			Queue<Student> studentQueue = new LinkedQueue<Student>();
 			int totalStudent = studentSheet.getRows() - 1;
-			int totalPreference = studentSheet.getColumns() - 1;
-			List<String> preferences = new ArrayList<String>();
 			for (int i = 1; i <= totalStudent; i++) {
+				List<String> preferences = new ArrayList<String>();
 				Cell[] s = studentSheet.getRow(i);
 				for (int j = 1; j < s.length; j++) {
 					preferences.add(s[j].getContents());
 				}
 				studentQueue.enqueue(new Student(s[0].getContents(),
 						preferences));
-				System.out.println(preferences);
-				preferences.clear();
 			}
 
 			for (int i = 1; i <= totalStudent; i++) {
 				Student student = studentQueue.dequeue();
-				System.out.println(student.getProgramPreference());
 				for (String preference : student.getProgramPreference()) {
 					int programCapacity = programs.get(preference);
-					System.out.println(programCapacity);
 					if (programCapacity != 0) {
 						student.setAllotedProgram(preference);
 						programs.put(preference, programCapacity - 1);
@@ -62,31 +57,27 @@ public class Counseling {
 				allotedProgram.put(student.getName(),
 						student.getAllotedProgram());
 			}
-
-			// WritableWorkbook workbook = Workbook.createWorkbook(new File(
-			// "C:/Users/User22/Downloads/alloted_sheet.xls"));
-			// WritableSheet allotedSheet = workbook.createSheet("sheet1", 0);
-			//
-			// for (int i = 0; i < allotedProgram.size(); i++) {
-			// for (String studentName : allotedProgram.keySet()) {
-			// Label l1 = new Label(0, i, studentName);
-			// allotedSheet.addCell(l1);
-			//
-			// Label l2 = new Label(1, i, allotedProgram.get(studentName));
-			// allotedSheet.addCell(l2);
-			// }
-			// }
-			// workbook.write();
-			// workbook.close();
-
+			generateExcel(allotedProgram);
 		} catch (Exception e) {
 			System.out.println(e.getMessage());
 		}
 		return allotedProgram;
 	}
 
-	public static void main(String[] args) {
-		new Counseling()
-				.allotProgram("C:/Users/User22/Downloads/course_data_complete.xls");
+	private void generateExcel(Map<String, String> allotedProgram) throws IOException, RowsExceededException, WriteException {
+		WritableWorkbook workbook = Workbook.createWorkbook(new File("C:/Users/User22/Downloads/alloted_sheet.xls"));
+		WritableSheet allotedSheet = workbook.createSheet("sheet1", 0);
+		List<String> sortedKeys = new ArrayList<>(allotedProgram.keySet());
+		Collections.sort(sortedKeys);
+		for (int i = 0; i < sortedKeys.size(); i++) {
+			String studentName = sortedKeys.get(i);
+			Label l1 = new Label(0, i, studentName);
+			allotedSheet.addCell(l1);
+
+			Label l2 = new Label(1, i, allotedProgram.get(studentName));
+			allotedSheet.addCell(l2);
+		}
+		workbook.write();
+		workbook.close();
 	}
 }
