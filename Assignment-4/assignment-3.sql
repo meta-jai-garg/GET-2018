820d2ddac945eb2ce68b3ebadb0037a8b7add7c2
meta-jai-garg <41181707+meta-jai-garg@users.noreply.github.com>
2018-08-21 11:37:11 +0530

    DBMS-Session-4-Assignment-4

--- Assignment-4/assignment-3.sql
+++ Assignment-4/assignment-3.sql
@@ -1,11 +1,9 @@
--- adding index on product table
+/*
+There is no need to create index in product
+and category relation as both the relation have primary key to uniquely identify a tuple
+so it make no sense to create duplicate primary key as index.
+*/
 
-CREATE INDEX `product_index` ON `product`(`title`);
 
--- adding index on product table
-
-CREATE INDEX `product_index` ON `product`(`title`);
-
--- adding index on product table
-
-CREATE INDEX `product_index` ON `product`(`title`);
+-- adding index to product_order_table
+ALTER TABLE product_order ADD INDEX order_date_index (order_date);
