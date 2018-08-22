abb090ce090811e8e57f017e8133e85b0377a322
meta-jai-garg <41181707+meta-jai-garg@users.noreply.github.com>
2018-08-22 12:08:50 +0530

    DBMS-Session-5-Assignment-5

--- DBMS/Assignment-5/OrderPojo.java
+++ DBMS/Assignment-5/OrderPojo.java
@@ -1,31 +1,40 @@
 import java.sql.Date;
 
-
+/**
+ * POJO to store order details such as orderId, orderDate, amount.
+ */
 public class OrderPojo {
 	private int orderId;
 	private Date orderDate;
 	private double amount;
+
 	public int getOrderId() {
 		return orderId;
 	}
+
 	public Date getOrderDate() {
 		return orderDate;
 	}
+
 	public double getAmount() {
 		return amount;
 	}
+
 	public void setOrderId(int orderId) {
 		this.orderId = orderId;
 	}
+
 	public void setOrderDate(Date orderDate) {
 		this.orderDate = orderDate;
 	}
+
 	public void setAmount(double amount) {
 		this.amount = amount;
 	}
+
 	@Override
 	public String toString() {
 		return "OrderPojo [orderId=" + orderId + ", orderDate=" + orderDate
 				+ ", amount=" + amount + "]";
-	}	
+	}
 }
