d173ae24c3efa22595cab163d9ae1662607cc336
meta-jai-garg <41181707+meta-jai-garg@users.noreply.github.com>
2018-08-22 12:10:04 +0530

    DBMS-Session-5-Assignment-5

--- DBMS/Assignment-5/TestQueryClass.java
+++ DBMS/Assignment-5/TestQueryClass.java
@@ -1,13 +1,43 @@
 import static org.junit.Assert.*;
 
+import java.util.List;
+
 import org.junit.Test;
 
-
 public class TestQueryClass {
-
+	/**
+	 * method to test orderByUserID()
+	 */
 	@Test
-	public void test() {
-		fail("Not yet implemented");
+	public void testOrderByUserID() {
+		List<OrderPojo> resultantPojo = new QueryClass().orderByUserID(34);
+		System.out.println(resultantPojo.toString());
 	}
 
+	/**
+	 * method to test imageBatchInsert()
+	 */
+	@Test
+	public void testImageBatchInsert() {
+		int result = new QueryClass().imageBatchInsert();
+		assertTrue(result == 5);
+	}
+
+	/**
+	 * method to test updateProductState
+	 */
+	@Test
+	public void testUpdateProductState() {
+		int result = new QueryClass().updateProductState();
+		assertTrue(result == 0);
+	}
+
+	/**
+	 * method to test categoryTitle()
+	 */
+	@Test
+	public void testCategoryTitle() {
+		List<CategoryPojo> resultantPojo = new QueryClass().categoryTitle();
+		System.out.println(resultantPojo.toString());
+	}
 }
