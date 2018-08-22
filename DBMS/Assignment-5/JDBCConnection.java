b2744743870d81fc23460ad5303afa810c189aa2
meta-jai-garg <41181707+meta-jai-garg@users.noreply.github.com>
2018-08-22 12:08:12 +0530

    DBMS-Session-5-Assignment-5

--- DBMS/Assignment-5/JDBCConnection.java
+++ DBMS/Assignment-5/JDBCConnection.java
@@ -2,18 +2,28 @@ import java.sql.Connection;
 import java.sql.DriverManager;
 import java.sql.SQLException;
 
-
+/**
+ * Helper class to make JDBC Connection
+ */
 public class JDBCConnection {
 	private static final String DB_DRIVER = "com.mysql.jdbc.Driver";
 	private static final String DB_NAME = "storefront";
-	private static final String DB_URL = "jdbc:mysql://localhost:3306/"+DB_NAME;
+	private static final String DB_URL = "jdbc:mysql://localhost:3306/"	+ DB_NAME;
 	private static final String DB_USERNAME = "root";
 	private static final String DB_PASSWORD = "12345";
-	 public static Connection getConnection(){
-		 Connection conn = null;
-		 try {
+
+	/**
+	 * Method to create connection
+	 * 
+	 * @return Connection object
+	 * @throws ClassNotFoundException, {@link SQLException}
+	 */
+	public static Connection getConnection() {
+		Connection conn = null;
+		try {
 			Class.forName(DB_DRIVER);
-			conn = DriverManager.getConnection(DB_URL, DB_USERNAME, DB_PASSWORD);
+			conn = DriverManager
+					.getConnection(DB_URL, DB_USERNAME, DB_PASSWORD);
 		} catch (ClassNotFoundException e) {
 			e.printStackTrace();
 			System.exit(1);
@@ -21,6 +31,6 @@ public class JDBCConnection {
 			e.printStackTrace();
 			System.exit(2);
 		}
-		 return conn;
-	 }
+		return conn;
+	}
 }
